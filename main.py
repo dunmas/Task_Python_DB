44aa464e9de51e6be50c69b872c9e18059d501f5
Danila <levafork@gmail.com>
2023-02-20 12:12:25 +0300

    Добавил рыбу

---
+++ main.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+def create_db(conn):
+    pass
+
+def add_client(conn):
+    pass
+
+def add_phone(conn):
+    pass
+
+def client_edit(conn):
+    pass
+
+def delete_phone(conn):
+    pass
+
+def delete_client(conn):
+    pass
+
+def find_client(conn):
+    pass
