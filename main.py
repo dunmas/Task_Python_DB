18ed344a894faf1f021b572da6130e4228af800a
Danila <levafork@gmail.com>
2023-02-20 14:15:21 +0300

    Реализовал функцию изменения данных о существующем клиенте

--- main.py
+++ main.py
@@ -53,20 +53,66 @@ def client_id_checker(conn, client_id):
 
 
 def add_phone(conn, client_id, phone):
-    existing_result = client_id_checker(conn, client_id)
+    if client_id_checker(conn, client_id) is None:
+        return
 
     with conn.cursor() as cur:
-        if existing_result is not None:
-            cur.execute("""
-            INSERT INTO phone_number(phone, client_id)
-            VALUES(%s, %s);
-            """, (phone, client_id))
+        cur.execute("""
+        INSERT INTO phone_number(phone, client_id)
+        VALUES(%s, %s);
+        """, (phone, client_id))
 
         conn.commit()
 
 
-def client_edit(conn):
-    pass
+def client_edit(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+    if client_id_checker(conn, client_id) is None:
+        return
+
+    if first_name is not None:
+        with conn.cursor() as cur:
+            cur.execute("""
+            UPDATE client
+            SET first_name = %s
+            WHERE client_id = %s;
+            """, (first_name, client_id))
+
+            conn.commit()
+
+    if last_name is not None:
+        with conn.cursor() as cur:
+            cur.execute("""
+            UPDATE client
+            SET last_name = %s
+            WHERE client_id = %s;
+            """, (last_name, client_id))
+
+            conn.commit()
+
+    if email is not None:
+        with conn.cursor() as cur:
+            cur.execute("""
+            UPDATE client
+            SET email = %s
+            WHERE client_id = %s;
+            """, (email, client_id))
+
+            conn.commit()
+
+    if phones is not None:
+        with conn.cursor() as cur:
+            cur.execute("""
+            DELETE FROM phone_number
+            WHERE client_id = %s;
+            """, (client_id,))
+
+            for phone in phones:
+                cur.execute("""
+                INSERT INTO phone_number(phone, client_id)
+                VALUES(%s, %s)
+                """, (phone, client_id))
+
+        conn.commit()
 
 def delete_phone(conn):
     pass
@@ -83,4 +129,14 @@ with psycopg2.connect(database='clients_db', user='postgres', password='zemege50
     # add_client(conn, 'Post', 'Gre', 'sql@yandex.ru')
     # add_client(conn, 'Saul', 'Goodman', 'sgalb@yahoo.com', ['9876543211', '9871234566'])
     # add_phone(conn, 1, '9876546782')
+
+    # Проверка функции edit
+    # client_edit(conn, 1, 'Anna')
+    # client_edit(conn, 1, None, 'Braun')
+    # client_edit(conn, 1, None, None, 'braun@yandex.ru')
+    # client_edit(conn, 1, None, None, None, ['1111111111', '2222222222'])
+
+    # Если такого ID нет - ничего не происходит
+    # client_edit(conn, 999, 'Bill')
+
 conn.close()
